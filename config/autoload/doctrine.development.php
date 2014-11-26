e213eabfc5810a56895aab63cfa20c1b18bf38cd
Bill Morrison <bill@thedesignzoo.co.uk>
2014-11-26 22:49:39 +0000

    rem details

--- config/autoload/doctrine.development.php
+++ config/autoload/doctrine.development.php
@@ -1,15 +1,21 @@
 <?php
 
+// get the db details from Env Vars on server - this is good for security
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
 return array(
   'doctrine' => array(
     'connection' => array(
       'orm_default' => array(
         'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
         'params' => array(
-          'host'     => 'us-cdbr-iron-east-01.cleardb.net',
-          'user'     => 'b709c791c0e243',
-          'password' => '2bab4a84',
-          'dbname'   => 'heroku_d609a9367feb337',
+          'host'     => $host,
+          'user'     => $user,
+          'password' => $password,
+          'dbname'   => $dbname,
         )
       )
     )
