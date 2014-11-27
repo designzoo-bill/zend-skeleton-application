5ed4521c24423c1b718ed54531da5f81a5c86c7a
Bill Morrison <bill@thedesignzoo.co.uk>
2014-11-27 00:01:29 +0000

    send email

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -14,10 +14,39 @@ use Zend\View\Model\ViewModel;
 use Zend\View\Model\JsonModel;
 use Doctrine\ORM\EntityManager;
 
+// Mail
+use Zend\Mail\Message;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
+
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+    	$this->sendEmail();
+    }
 
+    private function sendEmail ()
+    {
+		$message = new Message();
+		$message->addTo('matthew@zend.com')
+        	->addFrom('ralph.schindler@zend.com')
+        	->setSubject('Greetings and Salutations!')
+        	->setBody("Sorry, I'm going to be late today!");
+
+		// Setup SMTP transport using LOGIN authentication
+		$transport = new SmtpTransport();
+		$options   = new SmtpOptions(array(
+		    //'name'              => 'localhost.localdomain',
+		    'host'              => 'mailtrap.io',
+		    'port'				=> 465,
+		    'connection_class'  => 'login',
+		    'connection_config' => array(
+		        'username' => '2600189c78ecf24bd',
+		        'password' => 'ffe778660e905d',
+		    ),
+		));
+		$transport->setOptions($options);
+		$transport->send($message);
     }
 }
