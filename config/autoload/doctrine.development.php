9aac2dd6a38533540cee575f7bd1e035b3e22477
Bill Morrison <bill@thedesignzoo.co.uk>
2014-12-01 20:31:31 +0000

    test

--- config/autoload/doctrine.development.php
+++ config/autoload/doctrine.development.php
@@ -3,7 +3,7 @@
 // get the db details from Env Vars on server - this is good for security
 $host         = getenv('DB_HOST');
 $user         = getenv('DB_USER');
-$DB_PASSWORD  = getenv('DB_PASSWORD');
+$password     = getenv('DB_PASSWORD');
 $dbname       = getenv('DB_NAME');
 
 return array(
